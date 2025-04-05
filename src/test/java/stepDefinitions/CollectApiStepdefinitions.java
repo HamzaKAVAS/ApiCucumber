219145a51803f2c9f9be42c31822559c2db7d414
HamzaKAVAS <hmzkavas@gmail.com>
2025-04-05 21:47:25 +0300

    050425

--- src/test/java/stepDefinitions/CollectApiStepdefinitions.java
+++ src/test/java/stepDefinitions/CollectApiStepdefinitions.java
@@ -2,6 +2,7 @@ package stepDefinitions;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import utilities.ConfigReader;
 
@@ -18,5 +19,15 @@ public class CollectApiStepdefinitions {
     @Then("kullanici sorgu icin pathparametresi olarak {string} girer")
     public void kullanici_sorgu_icin_pathparametresi_olarak_girer(String pathParams) {
         url = url + "/" + pathParams;
+    }
+
+    @Then("kullanici query parametresi olark il degerine {string} ilce degerine {string} girer")
+    public void kullanici_query_parametresi_olark_il_degerine_ilce_degerine_girer(String il, String ilce) {
+        url = url + "?il=" + il + "&ilce=" + ilce;
+    }
+
+    @Then("kullanici gereki header degerleri ile bir GET request yapar")
+    public void kullanici_gereki_header_degerleri_ile_bir_get_request_yapar() {
+        response = RestAssured.given().header("authorization", ConfigReader.getProperty("CollectApiKey")).when().get(url);
     }
 }
