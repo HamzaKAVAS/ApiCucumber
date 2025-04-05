b2ef46d8a2e909e77bba4d303543824088892bb1
HamzaKAVAS <hmzkavas@gmail.com>
2025-04-05 21:39:49 +0300

    050425

--- src/test/java/stepDefinitions/CollectApiStepdefinitions.java
+++ src/test/java/stepDefinitions/CollectApiStepdefinitions.java
@@ -1,9 +1,22 @@
 package stepDefinitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.restassured.response.Response;
+import utilities.ConfigReader;
 
 public class CollectApiStepdefinitions {
 
     String url = "";
     Response response;
+
+    @Given("kullanici sorgu icin {string} adresine gider")
+    public void kullanici_sorgu_icin_adresine_gider(String configBaseUrl) {
+        url += ConfigReader.getProperty(configBaseUrl);
+    }
+
+    @Then("kullanici sorgu icin pathparametresi olarak {string} girer")
+    public void kullanici_sorgu_icin_pathparametresi_olarak_girer(String pathParams) {
+        url = url + "/" + pathParams;
+    }
 }
