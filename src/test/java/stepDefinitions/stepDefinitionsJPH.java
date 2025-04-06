4428b96302c00fda1fa9f62ef9dff10971843822
HamzaKAVAS <hmzkavas@gmail.com>
2025-04-06 22:38:30 +0300

    060425

--- src/test/java/stepDefinitions/stepDefinitionsJPH.java
+++ src/test/java/stepDefinitions/stepDefinitionsJPH.java
@@ -1,6 +1,7 @@
 package stepDefinitions;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.json.JSONObject;
@@ -16,5 +17,10 @@ public class stepDefinitionsJPH {
     @Given("Kullanici {string} adresine gider")
     public void kullanici_adresine_gider(String configBaseUrl) {
         url += ConfigReader.getProperty(configBaseUrl);
+    }
+
+    @Then("Kullanici path parametresi olarak {string} tanimlar")
+    public void kullanici_path_parametresi_olarak_tanimlar(String pathParams) {
+        url = url + "/" + pathParams;
     }
 }
