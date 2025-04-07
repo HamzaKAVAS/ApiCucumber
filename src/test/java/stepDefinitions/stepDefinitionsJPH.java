dee417b76a6437ebc122f17ed54a4bd72abf4ca0
HamzaKAVAS <hmzkavas@gmail.com>
2025-04-07 18:04:07 +0300

    070425

--- src/test/java/stepDefinitions/stepDefinitionsJPH.java
+++ src/test/java/stepDefinitions/stepDefinitionsJPH.java
@@ -5,7 +5,10 @@ import io.cucumber.java.en.Then;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.json.JSONObject;
+import org.junit.jupiter.api.Assertions;
 import utilities.ConfigReader;
+
+import static io.restassured.RestAssured.given;
 
 public class stepDefinitionsJPH {
 
@@ -22,5 +25,15 @@ public class stepDefinitionsJPH {
     @Then("Kullanici path parametresi olarak {string} tanimlar")
     public void kullanici_path_parametresi_olarak_tanimlar(String pathParams) {
         url = url + "/" + pathParams;
+    }
+
+    @Then("Kullanici verilen EndPointe bir GET request yapar ve Header degerlerini kaydeder")
+    public void kullanici_verilen_end_pointe_bir_get_request_yapar_ve_header_degerlerini_kaydeder() {
+        response = given().when().get(url);
+    }
+
+    @Then("Kullanici donen response'un durum kodunun {int} oldugunu dogrular")
+    public void kullanici_donen_response_un_durum_kodunun_oldugunu_dogrular(Integer expStatusCode) {
+        Assertions.assertEquals(expStatusCode,response.getStatusCode());
     }
 }
